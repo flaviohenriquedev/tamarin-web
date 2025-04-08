fe6d4ad7d2f44f21f2f01084a897ea5b5da4b95b
Flavio Henrique <flavio.henrique.dev@gmail.com>
2025-04-08 19:30:05 -0300

    ajustes

--- src/app/(autenticado)/adm/cadastro-usuario/page.tsx
+++
@@ -1,7 +0,0 @@
-import {UsuarioPaginaInicial} from "@/sistema/recursos-humanos/modulos/usuario/usuario-pagina-inicial";
-
-export default function PaginaCadastroUsuario() {
-    return (
-        <UsuarioPaginaInicial />
-    )
-}
---
+++ src/app/(autenticado)/adm/usuario/loading.tsx
@@ -0,0 +1,5 @@
+import {Loading} from "@/componentes/data-display/loading/loading";
+
+export default function LoadingAdmin() {
+    return <Loading />
+}
